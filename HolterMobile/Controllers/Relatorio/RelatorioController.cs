cfc4622b2e39239c36102cc8ddd35835c2017401
Matrodrigues <mffr94@gmail.com>
2016-07-03 17:53:00 -0300

    Criação da master page. Inclusão das classes do bootstrap

---
+++ HolterMobile/Controllers/Relatorio/RelatorioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HolterMobile.Controllers.Relatorio
+{
+    public class RelatorioController : Controller
+    {
+        // GET: Relatorio
+        public ActionResult Relatorio()
+        {
+            return View();
+        }
+    }
+}
--- HolterMobile/Models/Monitoramento.cs
+++ HolterMobile/Models/Monitoramento.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
+ 
 namespace HolterMobile.Models
 {
     [Table("TB_MONITORAMENTO")]
