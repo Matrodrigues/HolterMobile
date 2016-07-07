bddf7d9d2aa0b25c0f7a406d34ab79dbfe2412d6
Matrodrigues <mffr94@gmail.com>
2016-07-07 01:12:52 -0300

    .

---
+++ HolterMobile/Controllers/Perfil/PerfilController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HolterMobile.Controllers.Perfil
+{
+    public class PerfilController : Controller
+    {
+        // GET: Perfil
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+    }
+}
