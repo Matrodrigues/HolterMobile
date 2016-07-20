2d42cc18075f0a8c30c99abb5cc7185bc118a63a
Matrodrigues <mffr94@gmail.com>
2016-07-19 22:36:01 -0300

    Tela de cadastro de paciente. Validação client side com bootstrap.

---
+++ HolterMobile/Models/ViewModel/CadastrarPacienteVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HolterMobile.Models.ViewModel
+{
+    public class CadastrarPacienteVM : BaseVM
+    {
+        [Required(ErrorMessage = "Favor inserir o nome")]
+        public string primeiroNome { get; set; }
+
+        [Required(ErrorMessage = "Favor inserir o sobrenome")]
+        public string sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Favor selecione um sexo")]
+        public Char sexo { get; set; }
+        
+        [Required(ErrorMessage = "Favor inserir a data de nascimento.")]
+        [DataType(DataType.Date, ErrorMessage = "Data inválida")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? dataNascto { get; set; }
+    }
+}
